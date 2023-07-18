5b0f989565f7251a1d32abf0cf774e12d65d042d
Tehilla  Wolff <twolff@makor-capital.com>
2023-07-18 19:40:51 +0300

    first few assignments

--- js/strings/app.js
+++ js/strings/app.js
@@ -1,1 +1,48 @@
-console.log("ran")
+// 1.
+function toTitleCase(name) {
+        let sentence = name.split(" ");
+        for (let i = 0; i < sentence.length; i++) {
+            sentence[i] = sentence[i].charAt(0).toUpperCase() + sentence[i].slice(1);
+        }
+        return sentence.join(" ");    
+}
+
+console.log(toTitleCase('hello my name is tehilla and i d dd d'));
+
+//2.
+function searchInventory(inventory, query) {
+    for (let item of inventory) {
+        if (item.includes(query)) {
+            const itemArray = [item];
+            return itemArray;
+        } else {
+            `${query} does not exist in inventory`
+        }
+
+    }
+}
+
+console.log(searchInventory(["magic sword", "white knife", "baseball bat"], "white"));
+
+// 3
+function formatNPCName(lastName, firstName) {
+    const swappedPlace = `${firstName} ${lastName}` 
+    return swappedPlace
+}
+
+console.log(formatNPCName("wolff", "tehilla"));
+
+// 4
+
+function parseDuration(time) {
+    const seperatedTimes = time.split;
+    for (time of seperatedTimes) {
+        if (time.includes(h)) {
+            
+        } else if (time.includes(min)){
+
+        } else if (time.includes(sec)) {
+
+        }
+    }
+}
