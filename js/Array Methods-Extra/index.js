5bb81ac891ee6d8a6a6a7d7a3afa73877915cf7d
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-07-20 15:09:21 +0300

    Array Methods Extra - In Progress

--- js/Array Methods-Extra/index.js
+++ js/Array Methods-Extra/index.js
@@ -5,9 +5,26 @@ const characters = [
   { name: "Doe", level: 2 },
 ];
 
-function groupCharactersByLevel(characters) {
-    const characterByLevel = {};
-    characters.forEach((obj) => {
-        
-    })
-}
+const groupCharactersByLevel = (characters) => {
+  const characterByLevel = {};
+  characters.forEach((character) => {
+    const { level, name } = character;
+    if (!characterByLevel[level]) {
+      characterByLevel[level] = [];
+    }
+    characterByLevel[level].push({ name, level });
+  });
+
+  return characterByLevel;
+};
+console.log(groupCharactersByLevel(characters));
+
+// Exercise 2 - highest level character----------------------------------------------------------------------------------------
+const characters2 = [    { name: 'John', level: 2 },    { name: 'Jane', level: 5 }, { name: 'Daniel', level: 3 }, { name: 'Tal', level: 4 } ];
+ function highestLevelCharacter(characters){
+    return [...characters].sort((a,b) => a.level - b.level)[characters.length-1];
+ }
+
+  
+
+console.log(highestLevelCharacter(characters2));
