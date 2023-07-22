647560f648658a66748f5b288f412f044c4054aa
brainstromsurfer <wishmeheaven@gmail.com>
2023-07-22 11:42:40 +0300

    array_methods_extra-ex1_as_fn

--- js/3-array_methods/extra/app.js
+++ js/3-array_methods/extra/app.js
@@ -1,17 +1,25 @@
 const characters = require('../extra/data/mockData')
 //  1. Group Characters by Level
 
-let uniqueObj = {}
-
-for (const {name, level} of characters) {
-    if (!uniqueObj[level]) {
-        uniqueObj[level] = [{         
-            name: name,
-            level: level        
-        }]
-    } else {
-        uniqueObj[level].push({name: name, level: level})       
+const groupCharactersByLevel = (characters) => {
+    
+    let uniqueObj = {}
+    
+    for (const {name, level} of characters) {
+        if (!uniqueObj[level]) {
+            uniqueObj[level] = [{         
+                name: name,
+                level: level        
+            }]
+        } else {
+            uniqueObj[level].push({name: name, level: level})       
+            }
         }
-    }
+    return uniqueObj
+}
 
-console.log(uniqueObj);
+console.log(groupCharactersByLevel(characters));
+
+
+// 2. Find Highest Level Character
+
