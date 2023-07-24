4d6f55ccd4d357d33876371424106897720b8f61
brainstromsurfer <wishmeheaven@gmail.com>
2023-07-24 12:10:51 +0300

    array-methods_extra-ex2_refactor-into-a-while-loop

--- js/3-array_methods/extra/data/mockData.js
+++ js/3-array_methods/extra/data/mockData.js
@@ -1,6 +1,6 @@
 const characters = [
   { name: 'John', level: 2, skills: ['fire', 'ice'] },
-  { name: 'Jane', level: 5, skills: ['fire', 'ice', 'wind'] },
+  { name: 'Jane', level: 8, skills: ['fire', 'ice', 'wind'] },
   { name: 'Doe', level: 2, skills: ['earth'] },
 ];
 
