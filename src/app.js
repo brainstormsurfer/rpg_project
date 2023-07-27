5ffe18f673776cc26c18145ae48432d1dc682fc7
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-07-27 16:16:58 +0300

    testing merging

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
 // Exercise 1 ---------------------------------
-
-const gameWorld = {
-  players: [
-    {
-      name: "tehilla",
-      health: 2,
-      position: { x: 2, y: 5 },
-      inventory: ["sword", "health potion"],
-    },
-    {
-      name: "shady",
-      health: 4,
-      position: { x: 3, y: 6 },
-      inventory: ["axe", "shield"],
-    },
-    {
-      name: "daniel",
-      health: 6,
-      position: { x: 2, y: 4 },
-      inventory: ["bow", "arrows"],
-    },
-    {
-      name: "ran",
-      health: 8,
-      position: { x: 3, y: 5 },
-      inventory: ["dagger", "spear"],
-    },
-  ],
-
+const game = {
+  gameWorld: {
+    players: [
+      {
+        name: "tehilla",
+        health: 2,
+        position: { x: 2, y: 5 },
+        inventory: ["sword", "health potion"],
+      },
+      {
+        name: "shady",
+        health: 4,
+        position: { x: 3, y: 6 },
+        inventory: ["axe", "shield"],
+      },
+      {
+        name: "daniel",
+        health: 6,
+        position: { x: 2, y: 4 },
+        inventory: ["bow", "arrows"],
+      },
+      {
+        name: "ran",
+        health: 8,
+        position: { x: 3, y: 5 },
+        inventory: ["dagger", "spear"],
+      },
+    ],
+  },
   enemies: [
     { name: "orc", health: 2, position: { x: 2, y: 5 } },
     { name: "troll", health: 2, position: { x: 4, y: 5 } },
@@ -41,7 +41,7 @@ const gameWorld = {
     { name: "shield", effect: "block" },
     { name: "crossbow", effect: "range" },
   ],
-  
+
   npcs: [],
 
   //   Exercise 2 ------
@@ -69,4 +69,8 @@ const gameWorld = {
   removeItem: function (itemName) {
     this.items = this.items.filter((item) => item.name !== itemName);
   },
+
+  // Exercise 3 -------------------------------------------------------------------------------
+
+  
 };
