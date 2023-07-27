5f618197615f6e1addfbcbcfd5a67646878d9295
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-07-27 13:51:06 +0300

    pshed

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// Exercise 1 ---------------------------------
+const gameWorld = {
+  players: [
+    { name: "tehilla" },
+    { health: 2 },
+    { position: { x: 2, y: 5 } },
+    { inventory: ["lolo", "kiki"] },
+  ],
+  enemies: [
+    { name: "moster" },
+    { health: 2 },
+    { position: { x: 2, y: 5 } },
+    { inventory: ["lolo", "kiki"] },
+  ],
+  items: [{ name: "stick" }, { effect: "bash" }],
+  npcs: [],
+};
