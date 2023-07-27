48165eca51e1c1c50b48248fe4b2d73783c32020
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-07-27 14:22:44 +0300

    pshed

--- src/app.js
+++ src/app.js
@@ -1,17 +1,36 @@
 // Exercise 1 ---------------------------------
 const gameWorld = {
   players: [
-    { name: "tehilla" },
-    { health: 2 },
-    { position: { x: 2, y: 5 } },
-    { inventory: ["lolo", "kiki"] },
+    {
+      name: "tehilla",
+      health: 2,
+      position: { x: 2, y: 5 },
+      inventory: ["lolo", "kiki"],
+    },
   ],
   enemies: [
-    { name: "moster" },
-    { health: 2 },
-    { position: { x: 2, y: 5 } },
-    { inventory: ["lolo", "kiki"] },
+    {
+      name: "moster",
+      health: 2,
+      position: { x: 2, y: 5 },
+      inventory: ["lolo", "kiki"],
+    },
   ],
-  items: [{ name: "stick" }, { effect: "bash" }],
+  items: [
+    {
+      name: "stick",
+      effect: "bash",
+    }
+  ],
   npcs: [],
+
+  //   Exercise 2 ------
+
+  addPlayer: function (player) {
+    this.players.push(player);
+  },
+
+  removePlayer: function (player) {
+    
+  },
 };
