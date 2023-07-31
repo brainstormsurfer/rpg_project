250555ef410b594f212f08d8b8d8a0f467449260
twolff-makor <107106896+twolff-makor@users.noreply.github.com>
2023-07-31 20:04:52 +0300

    assignment one through three

---
+++ js/callbackFunctions/index.js
@@ -0,0 +1,52 @@
+// 1
+function processEnemies(arr, callback) {
+    let newEnemies = [];
+    if (Array.isArray(arr)) {
+        for (item of arr) {
+            if (item.name && item.health){
+                newEnemies.push(item);
+            } else {return `Enemy with ID of ${item.id} is not in the correct structure`}
+        }
+        newEnemies = newEnemies.map(item => callback(item));
+        return newEnemies;
+    } else {return 'Expected an array of enemies'};
+}
+
+// 2
+function processQuest(arr, callback) {
+    let newQuests = [];
+    if (Array.isArray(arr)) {
+        for (item of arr) {
+            if (item.name && item.experience){
+                newQuests.push(item);
+            } else {return `Quest with ID of ${item.id} is not in the correct structure`}
+        }
+        newQuests = newQuests.map(item => callback(item));
+        return newQuests;
+    } else {return 'Expected an array of quests'};
+}
+
+// 3
+function processPlayer(arr, callback) {
+    let newPlayers = [];
+    if (Array.isArray(arr)) {
+        for (item of arr) {
+            if (item.name && item.health && item.level && item.location && item.inventory){
+                newPlayers.push(item);
+            } else {return `Player with ID of ${item.id} is not in the correct structure`}
+        }
+        newPlayers = newPlayers.map(item => callback(item));
+        return newPlayers;
+    } else {return 'Expected an array of players'};
+}
+
+
+const enemyArrayObj = 
+[
+    {  id: 12,  name: "Java",  experience: 100, healh: 60,level : 90, location : 90, inventory : 90},
+    {  id: 7,  name: "Jane",   experience: 100, health: 60,level : 90, location : 90, inventory : 90},
+    {  id: 4,  name: "Troll",   experience: 100, health: 60,level : 90, location : 90, inventory : 90},
+    {  id: 15,  name: "Python",   experience: 100, health: 60,level : 90, location : 90, inventory : 90}
+]
+
+console.log(processPlayer(enemyArrayObj, console.log));
