894253d8fa41ef0bda1494032d217ef72ec51d55
brainstromsurfer <wishmeheaven@gmail.com>
2023-08-02 05:15:51 +0300

    closure_exercise-complete

--- js/7-closures/app.js
+++ js/7-closures/app.js
@@ -1,62 +1,94 @@
-
-
-
-
-
-
-
 const createPlayerCharacter = (characterName, initialLevel) => {
-    const character = {
-        name: characterName,
-        level: initialLevel,
-        xp: 0,
-        levelUp : function() {
-            this.level++
-           console.log(`${this.name} leveled up! current level: ${this.level}`)
-        },
-        addXp: function (updateXpBy) {
-            this.xp += updateXpBy
-            console.log(`Added ${updateXpBy}xp. ${this.name} current xp: ${this.xp}`)
-        }, 
-        inventory : {}
-    }
-    return character
-}
+  
+    
+    return {        
+    name: characterName,
+    level: initialLevel,
+    xp: 0,
+    levelUp : function() {
+      this.level++;
+      console.log(`${this.name} leveled up! Current level: ${this.level}`);
+    },
+    addXp: (updateXpBy) => {
+      this.xp += updateXpBy;
+      console.log(`Added ${updateXpBy}xp. ${this.name}'s xp: ${this.xp}`);
+    },
+  }
+
+};
 
 const createInventory = () => {
-    const inventory = {
-        items : [],
-        addItem : function(item) {
-            this.items.push(item)
-            console.log(`${item} has been added to inventory`);
-        },
-        removeItem : function(item) {
-            this.items = this.items.filter((itemToDelete) => item !== itemToDelete)
-            console.log(`${item} has been removed from inventory`);
-        },
-        getItems: function() {
-            return `Items: ${this.items}`
+  return {
+    items: [],
+    addItem: function (item) {
+      this.items.push(item);
+      console.log(`${item} has been added to inventory`);
+    },
+    removeItem: function (item) {
+      this.items = this.items.filter((itemToDelete) => item !== itemToDelete);
+      console.log(`${item} has been removed from inventory`);
+    },
+    getItems: function () {
+      return `Items: ${this.items}`;
+    },
+  };
+};
+
+const createAbility = (abilityName, power) => {
+    return {
+        name : abilityName,
+        power: power, 
+        useAbility : function() {
+            console.log(`Using ${this.power} ${this.name} power`);
         }
     }
-    return inventory
 }
 
-const player1 = createPlayerCharacter("Doobi", 10)
-console.log(player1);
-player1.levelUp()
-player1.levelUp()
-player1.addXp(5)
-player1.addXp(15)
-console.log(player1);
+const createEnemy = (enemyName, health) => {
+  return {
+    name: enemyName,
+    health: health,
+    attack: function () {
+      console.log("enemy is attacking");
+    },
+    reduceHealth: function (reduceHealthBy) {
+      this.health -= reduceHealthBy;
+      console.log(`${this.name} remain ${this.health} health`);
+    },
+    isDefeated: function () {
+      return this.health <= 0 ? true : false;
+    },
+  };
+};
 
-player1.inventory = createInventory()
-const {inventory} = player1
-inventory.addItem('axe')
-inventory.addItem('book')
-console.log(inventory.getItems())
-inventory.removeItem('book')
-console.log(inventory.getItems())
+const player1 = createPlayerCharacter("Krembo", 10);
+console.log("player1 stats before changes", player1);
+console.log("---------------------------");
+player1.levelUp();
+player1.addXp(5);
+player1.addXp(15);
+console.log("---------------------------");
+console.log("player1 stats after changes", player1);
+console.log("===========================");
 
+const inventory = createInventory();
+inventory.getItems();
+inventory.addItem("axe");
+inventory.addItem("book");
+console.log("inventory before remove an item", inventory.getItems());
+inventory.removeItem("book");
+console.log("inventory after removing book", inventory.getItems());
+console.log("============================");
 
+const shadow = createEnemy('dj shadow', 100)
+console.log(shadow);
+shadow.reduceHealth(99)
+console.log(shadow.isDefeated())
+shadow.reduceHealth(1)
+console.log(shadow.isDefeated())
+console.log("============================");
 
-
+const horsePower = createAbility('horse', 70)
+console.log(horsePower)
+horsePower.useAbility()
+console.log("============================");
