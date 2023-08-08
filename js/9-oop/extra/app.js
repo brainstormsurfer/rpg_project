584f876b2850ecd1ac61970cf1841bc8c4650b88
brainstromsurfer <wishmeheaven@gmail.com>
2023-08-08 13:19:31 +0300

    quest-reward_untested

--- js/9-oop/extra/app.js
+++ js/9-oop/extra/app.js
@@ -1,68 +1,91 @@
 class Character {
   #health;
+  quest = null;
+  abilities = [];
+  items = [];
+  gold = 0;
 
   constructor(name, health, strength, defense) {
     this.name = name;
     this.strength = strength;
     this.defense = defense;
-    this.#health = health >= 0 ? health : 0;
+    this.#health = health >= 0 ? health : 0;    
   }
-
   get health() {
     return this.#health;
   }
-
   // set health(health) {
   //     if (health < 0) {
   //         this.#health = 0
   //     } else this.#health = health;
   // }
-
   receiveDamage(damage) {
     this.#health = Math.max(0, this.#health - damage);
   }
-
   attack(target) {
     if (target instanceof Character && target !== this) {
         const result = Math.max(this.strength - target.defense, 0);
-        console.log("ATTACK (total damage)", result);
+        console.log(`${this.name} ATTACK ${target.name} (${target.defense} defense) with ${this.strength} strength`);
       target.receiveDamage(result);
     }
   }
-
   displayStats() {
-    const {name, health, strength, defense } = this;
+    const {health, strength, defense } = this;
     return `${this.characterType()} Current Stats -
                     Health: ${health}
                     Strength: ${strength}
                     Defense: ${defense}`;
   }
-
   characterType() {
     return `${this.name} is a basic character.`
   }
+  acceptNewQuest(quest) {
+    if (this.quest === null) {
+        this.quest = quest
+    }
+  }
+  receiveReward() {    
+    const {quest} = this.quest    
+    if (quest.completed) {      
+       if (quest.reward.abilities.length > 0) {        
+          this.abilities = quest.reward.abilities
+       }
+       if (quest.reward.items.length > 0) {        
+          this.items = quest.reward.items
+       }
+       if (quest.reward.gold > 0) {        
+          this.gold = quest.reward.gold
+       }
+    }  
+  }
+  getItems() {
+    return this.items.map(i => i.name)
+  }
+  getAbilities() {
+    return this.abilities.map(a => a.name)
+  }
+  getGold() {
+    return this.gold
+  }
 }
-
 
 class Knight extends Character {
     constructor(name, health, strength, defense, armor) {
         super(name, health, strength, defense);
         this.armor = armor;
     }
-
     shieldAttack(target) {
         if (target instanceof Character && target !== this) {
-            const result = Math.max(this.armor + this.strength - target.defense, 0)
-            console.log('SHIELD ATTACK (total damage)', result);
+          const totalAttack = this.armor + this.strength;
+            const result = Math.max(totalAttack - target.defense, 0)
+            console.log(`${this.name} SHIELD ATTACK of ${totalAttack} on ${target.name} (${target.defense} defense)`);
             target.receiveDamage(result)
         }
     }
-
     displayStats(){
         return `${super.displayStats()}
                     Armor:${this.armor}`
     } 
-
     characterType(){
         return `${this.name} is a knight.`
     }
@@ -75,49 +98,63 @@ class Knight extends Character {
     }
     castSpell(target) {
         if (target instanceof Character && target !== this && this.mana >= 10) {
-            const result = Math.max(this.mana + this.strength - target.defense, 0)
-            console.log('CASTING SPELL (total damage)', result);
+            const totalAttack = this.mana + this.strength
+            const result = Math.max(totalAttack - target.defense, 0)
+            console.log(`${this.name} CASTING SPELL of ${totalAttack} on ${target.name} (${target.defense} defense)`);
             this.mana = Math.max(this.mana - 10, 0);
             target.receiveDamage(result)
         }
     }
-    
     displayStats(){
         return `${super.displayStats()} 
                     Magic:${this.mana} mana`
     }
-
     characterType(){
         return `${this.name} is a Mage.`
     }
 }
 
+class Quest {
+    constructor(name, desc, reward, requiredEnemies, completed) {
+        this.name = name;
+        this.desc = desc;
+        this.reward = reward;
+        this.requiredEnemies = requiredEnemies;
+        this.completed = completed;
+    }
+    completeQuest() {
+        if (this.requiredEnemies.length === 0)    
+        this.completed = true
+    }
+}
+
+
 // ==============================
 
 // Create a new character
-const hero1 = new Character("Donatello", 20, 50, 15);
-const hero2 = new Character("Leonardo", 20, 50, 15);
+const hero1 = new Character("Donatello", 100, 30, 15);
+const hero2 = new Character("Leonardo", 100, 30, 15);
 
-// console.log(hero, "health: " + hero.health);
-//   console.log(`Character { name: ${hero.name},health: ${hero.health}, strength: ${hero.strength}, defense: ${hero.defense} }`)
-// hero2.displayStats();
-// hero1.attack(hero2);
-// hero2.displayStats();
+hero2.displayStats()
+console.log('----------');
+console.log(`hero1 (${hero1.name}) health`, hero1.health);
+hero2.attack(hero1);
+console.log(`hero1 (${hero1.name}) health`, hero1.health);
+console.log('==========');
 
 const hero3 = new Knight("Michelangelo", 20, 30, 50, 25);
 const hero4 = new Mage("Rafaello", 20, 30, 50, 25);
-// console.log(hero3);
-// console.log(hero4);
+
 hero3.displayStats()
 console.log('----------');
-console.log(hero3.displayStats());
+console.log(`hero3 (${hero3.name}) health`, hero3.health);
 hero4.castSpell(hero3);
-console.log(hero3.displayStats());
+console.log(`hero3 (${hero3.name}) health`, hero3.health);
 console.log('==========');
 
 console.log('----------');
-console.log(hero4.displayStats());
+console.log(`hero4 (${hero4.name}) health`, hero4.health);
 hero3.shieldAttack(hero4);
-console.log(hero4.displayStats());
+console.log(`hero4 (${hero4.name}) health`, hero4.health);
 console.log('==========');
 
