c9bddc901670288abf675664b0e0e2d382a1e4d1
brainstromsurfer <wishmeheaven@gmail.com>
2023-08-08 12:09:54 +0300

    knight_and_mage

--- js/9-oop/extra/app.js
+++ js/9-oop/extra/app.js
@@ -1,52 +1,123 @@
+class Character {
+  #health;
 
+  constructor(name, health, strength, defense) {
+    this.name = name;
+    this.strength = strength;
+    this.defense = defense;
+    this.#health = health >= 0 ? health : 0;
+  }
 
-class Character {    
+  get health() {
+    return this.#health;
+  }
 
-    #health;
+  // set health(health) {
+  //     if (health < 0) {
+  //         this.#health = 0
+  //     } else this.#health = health;
+  // }
 
-    constructor (name, health, strength, defense) {
-        this.name = name;
-        this.strength = strength;
-        this.defense = defense;
-        this.health = health >= 0 ? health : 0
+  receiveDamage(damage) {
+    this.#health = Math.max(0, this.#health - damage);
+  }
+
+  attack(target) {
+    if (target instanceof Character && target !== this) {
+        console.log("INSIDE", this.strength);
+      const result = Math.max(this.strength - target.defense, 0);
+      target.receiveDamage(result);
     }
+  }
 
-    get health() {
-        return this.#health
-    }
-
-    set health(health) {
-        if (health < 0) {
-            this.#health = 0
-        } else this.#health = health;
-    }
-
-    attack(target) {
-        target.health -= this.strength
-        // return target.health
-    }
-
-    receiveDamage(damage) {
-        this.#health = Math.max(0, this.#health-damage);
-    }
-
-    displayStats() {
-       const {name, health, strength, defense } = this;
-        console.log(`${name} current stats:
+  displayStats() {
+    const { name, health, strength, defense } = this;
+    return `${this.characterType()} Current Stats -
                     Health: ${health}
                     Strength: ${strength}
-                    Defense: ${defense}`);
-            }
+                    Defense: ${defense}`;
+  }
 
+  characterType() {
+    return `${this.name} is a basic character.`
+  }
 }
 
 
-  // Create a new character
-  const hero = new Character("Hero", 10, 20, 15);
+class Knight extends Character {
+    constructor(name, health, strength, defense, armor) {
+        super(name, health, strength, defense);
+        this.armor = armor;
+    }
 
-  console.log(hero, 'health: ' + hero.health)
+    shieldAttack(target) {
+        if (target instanceof Character && target !== this) {
+            const result = Math.max(this.armor + this.strength - target.defense, 0)
+            console.log('SHIELD ATTACK');
+            target.receiveDamage(result)
+        }
+    }
+
+    displayStats(){
+        return `${super.displayStats()}
+                    Armor:${this.armor}`
+    } 
+
+    characterType(){
+        return `${this.name} is a knight.`
+    }
+ }
+
+ class Mage extends Character {
+    constructor(name, health, strength, defense, mana) {
+        super(name, health, strength, defense);
+        this.mana = mana;
+    }
+    castSpell(target) {
+        if (target instanceof Character && target !== this && this.mana >= 10) {
+            const result = Math.max(this.mana + this.strength - target.defense, 0)
+            this.mana = Math.max(this.mana - 10, 0);
+            console.log('CASTING SPELL');
+            target.receiveDamage(result)
+        }
+    }
+    
+    displayStats(){
+        return `${super.displayStats()} 
+                    Magic:${this.mana} mana`
+    }
+
+    characterType(){
+        return `${this.name} is a Mage.`
+    }
+}
+
+// ==============================
+
+// Create a new character
+const hero1 = new Character("Donatello", 20, 50, 15);
+const hero2 = new Character("Leonardo", 20, 50, 15);
+
+// console.log(hero, "health: " + hero.health);
 //   console.log(`Character { name: ${hero.name},health: ${hero.health}, strength: ${hero.strength}, defense: ${hero.defense} }`)
-  hero.receiveDamage(10)
-  hero.displayStats()  
-  
- 
+// hero2.displayStats();
+// hero1.attack(hero2);
+// hero2.displayStats();
+
+const hero3 = new Knight("Michelangelo", 20, 30, 50, 25);
+const hero4 = new Mage("Rafaello", 20, 30, 50, 25);
+// console.log(hero3);
+// console.log(hero4);
+hero3.displayStats()
+console.log('----------');
+console.log(hero3.displayStats());
+hero4.castSpell(hero3);
+console.log(hero3.displayStats());
+console.log('==========');
+
+console.log('----------');
+console.log(hero4.displayStats());
+hero3.shieldAttack(hero4);
+console.log(hero4.displayStats());
+console.log('==========');
+
