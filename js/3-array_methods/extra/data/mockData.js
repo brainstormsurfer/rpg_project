f7a0b1ff792eac10887a11ad9da37b4a3994d922
shadinab <shadinabwani2000@gmail.com>
2023-08-10 16:11:31 +0300

    initial setup

--- js/3-array_methods/extra/data/mockData.js
+++
@@ -1,7 +0,0 @@
-const characters = [
-  { name: 'John', level: 2, skills: ['fire', 'ice'] },
-  { name: 'Jane', level: 8, skills: ['fire', 'ice', 'wind'] },
-  { name: 'Doe', level: 2, skills: ['earth'] },
-];
-
-module.exports = characters
