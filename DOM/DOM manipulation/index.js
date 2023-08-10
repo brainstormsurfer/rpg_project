1ccf6343e559f1bbbcd81c96a503a7b781711819
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-08-10 12:58:15 +0300

    DOM Exercises finished

--- DOM/DOM manipulation/index.js
+++ DOM/DOM manipulation/index.js
@@ -6,5 +6,12 @@ const newSubTitle = document.createElement('li')
 const subTitleText = document.createTextNode('sub title 4')
 newSubTitle.appendChild(subTitleText);
 myUl.appendChild(newSubTitle);
-
+const myLiParent = myLi.parentElement;
+myLiParent.lastElementChild.remove();
+const head = document.querySelector('head');
+const title = document.head.querySelector('title');
+title.textContent = 'DOM Master';
+const div = myLiParent.nextElementSibling
+const p = div.firstElementChild
+p.textContent = "Now I know how to traverse the DOM :D "
 
