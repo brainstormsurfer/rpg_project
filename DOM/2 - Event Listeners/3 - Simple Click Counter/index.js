3ec3ce25e1ea7a12f736dc0b386b227b93b3042e
brainstromsurfer <wishmeheaven@gmail.com>
2023-08-17 00:25:54 +0300

    DOM-2_event-listeners-ex3_click-counter_refactoring

--- DOM/2 - Event Listeners/3 - Simple Click Counter/index.js
+++ DOM/2 - Event Listeners/3 - Simple Click Counter/index.js
@@ -1,55 +1,62 @@
-const counterValue = document.querySelector('p')
+function createButtons() {
+  const plusBtn = document.querySelector("#clickButton");
+  const minusBtn = plusBtn.cloneNode(true);
+  const resetBtn = plusBtn.cloneNode(true);
 
-const plusBtn = document.querySelector('#clickButton')
-const minusBtn = plusBtn.cloneNode(true)
-const resetBtn = minusBtn.cloneNode(true)
+  const buttonsWrapper = document.createElement("div");
+  buttonsWrapper.style.display = "flex";
+  buttonsWrapper.style.justifyContent = "space-around";
+  plusBtn.parentElement.insertBefore(buttonsWrapper, counterValue);
+  buttonsWrapper.append(minusBtn, resetBtn, plusBtn);
 
-const buttonsWrapper = document.createElement("div")
-buttonsWrapper.style.display = 'flex'
-buttonsWrapper.style.justifyContent = 'space-around'
-plusBtn.parentElement.insertBefore(buttonsWrapper, counterValue)
-buttonsWrapper.append(minusBtn,resetBtn,plusBtn)
-
-const buttonPropsObj = {
-    'minus': {
-        textContent : '-',
-        borderRadius : '50%'
-    },
-    'reset': {
-        textContent : 'Reset',
-        borderRadius : '25px'
-    },
-    'plus': {
-        textContent : '+',
-        borderRadius : '50%'
-    }
-};
-
-const changeCounterValue = function() {
-    let currentValue = parseInt(counter.textContent)
-    switch (this.id) {
-        case 'reset': {
-            currentValue = 0
-        break;
-        }
-        case 'plus': {
-        currentValue++;
-        break;
-        }
-        case 'minus': {
-            currentValue--
-            break;
-        }
-    }
-    counter.textContent = currentValue    
+  return { minusBtn, resetBtn, plusBtn };
 }
 
-document.querySelectorAll('button').forEach((btn, index) => {
-    btn.addEventListener('click', changeCounterValue)    
-    const id = Object.keys(buttonPropsObj)[index]
-    const properties = buttonPropsObj[id]
-    btn.id = id;
-    btn.textContent = properties.textContent;
-    btn.style.borderRadius = properties.borderRadius;        
-})
+function setButtonsFunctionality(buttons) {
+  const buttonsPropsObj = {
+    minus: {
+      textContent: "-",
+      borderRadius: "50%",
+    },
+    reset: {
+      textContent: "Reset",
+      borderRadius: "25px",
+    },
+    plus: {
+      textContent: "+",
+      borderRadius: "50%",
+    },
+  };
 
+  const changeCounterValue = function () {
+    let currentValue = parseInt(counter.textContent);
+    switch (this.id) {
+      case "reset": {
+        currentValue = 0;
+        break;
+      }
+      case "plus": {
+        currentValue++;
+        break;
+      }
+      case "minus": {
+        currentValue--;
+        break;
+      }
+    }
+    counter.textContent = currentValue;
+  };
+
+  buttons.forEach((btn, index) => {
+    btn.addEventListener("click", changeCounterValue);
+    const id = Object.keys(buttonsPropsObj)[index];
+    const btnValues = buttonsPropsObj[id];
+    btn.id = id;
+    btn.textContent = btnValues.textContent;
+    btn.style.borderRadius = btnValues.borderRadius;
+  });
+}
+
+const counterValue = document.querySelector("p");
+const buttons = createButtons()
+setButtonsFunctionality(Object.values(buttons))
