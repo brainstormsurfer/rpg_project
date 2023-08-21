ae91bb5545ebcfb4a964f9a2c94b1dcbfd2d5228
twolff-makor <107106896+twolff-makor@users.noreply.github.com>
2023-08-21 12:19:55 +0300

    still not working

--- js/eventListeners/shapeDraw/index.js
+++ js/eventListeners/shapeDraw/index.js
@@ -3,12 +3,10 @@ const canvas = document.getElementById("drawingArea");
 const ctx = canvas.getContext("2d");
 
 function drawShape(shape) {
-    console.log("in shape")
     return (event) => {
         ctx.beginPath();
         if (shape === "rectangle") {
-            console.log("in rectangle");
-            ctx.rect(event.clientX, event.clientY, 200, 150);
+            ctx.rect(event.clientX, event.clientY, 80, 50);
         } else if (shape === "circle") {
             ctx.arc(event.clientX, event.clientY, 50, 0, 2 * Math.PI);
         }
@@ -18,9 +16,9 @@ function drawShape(shape) {
 
 tools.forEach((item) => {
     const shape = item.dataset.tool;
-    item.addEventListener("click", () => {
-        console.log(shape);
-        canvas.addEventListener("click", () => {drawShape(shape)});
+    item.addEventListener("click", (event) => {
+        canvas.addEventListener("click", drawShape(shape), {once : true});
     });
 });
+
 
