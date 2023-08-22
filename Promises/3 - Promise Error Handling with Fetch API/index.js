5f21b7d9a50f8303ccab29735bc1d86772d0c51e
brainstromsurfer <wishmeheaven@gmail.com>
2023-08-23 00:45:58 +0300

    Promises_ex3_error-handling

---
+++ Promises/3 - Promise Error Handling with Fetch API/index.js
@@ -0,0 +1,28 @@
+const usersUrl = "https://jsonplaceholder.typicode.com";
+const errorDiv = document.getElementById("error");
+
+fetch(`${usersUrl}/users`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Status Code Error: ${response.status}`);
+    }
+    return response.json();
+  })  
+.then((data) => {
+        console.log("FETCHED ALL USERS2", data);
+    const usersDiv = document.getElementById("users");
+    for (let user of data) {
+      const userDiv = document.createElement("div");
+      userDiv.classList.add("user");
+      userDiv.innerHTML = `
+         <h2>${user.name}</h2>
+         <p><strong>Email${user.email}</strong></p>         
+         `;
+         usersDiv.appendChild(userDiv);
+    }
+})
+  .catch((err) => {
+    console.error('Console\'s Error: ', err)
+    const error = document.querySelector('.error-message')
+    error.textContent = `An error occurred: ${err.message}. Please try again later`
+  });
